e955016f658d19a8465c00e928feff0e97383914
Alex's PC <zhangyh8216@sina.com>
2025-02-06 23:48:01 +0800

    First

---
+++ home/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def credits(request):
+    content = "Nicky\nYour Name"
+    return HttpResponse(content, content_type="text/plain")
+
+def news(request):
+    data = {
+        'news': [
+            "RiffMates now has a news page!",
+            "RiffMates has its first web page",
+        ],
+    }
+    return render(request, "news2.html", data)
