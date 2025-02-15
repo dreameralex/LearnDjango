ab431e477a16ba1ae175277baa95119ab3d55e70
Alex's PC <zhangyh8216@sina.com>
2025-02-15 23:49:26 +0800

    Finished 7.2.5

--- content/views.py
+++ content/views.py
@@ -1,8 +1,9 @@
 # RiffMates/content/views.py
 from django.core.mail import send_mail
-from django.shortcuts import render, redirect
-
-from content.forms import CommentForm
+from content.models import MusicianBandChoice, SeekingAd
+from content.forms import CommentForm, SeekingAdForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 
 def comment(request):
     if request.method == 'GET':
@@ -36,3 +37,55 @@ def comment_accepted(request):
         """
     }
     return render(request, "general.html", data)
+def list_ads(request):
+    data = {
+        'seeking_musician':SeekingAd.objects.filter(
+            seeking=MusicianBandChoice.MUSICIAN),
+        'seeking_band':SeekingAd.objects.filter(
+            seeking=MusicianBandChoice.BAND),
+    }
+    return render(request, "list_ads.html", data)
+
+@login_required
+def seeking_ad(request):
+    if request.method == 'GET':
+        form = SeekingAdForm()
+
+    else: # POST
+        form = SeekingAdForm(request.POST)
+
+        if form.is_valid():
+            ad = form.save(commit=False)
+            ad.owner = request.user
+            ad.save()
+            return redirect("list_ads")
+
+    # Was a GET, or Form was not valid
+    data = {
+        "form": form,
+    }
+
+    return render(request, "seeking_ad.html", data)
+
+@login_required
+def seeking_ad(request, ad_id=0):
+    if request.method == 'GET':
+        if ad_id == 0:
+            form = SeekingAdForm()
+        else:
+            ad = get_object_or_404(SeekingAd, id=ad_id, owner=request.user)
+            form = SeekingAdForm(instance=ad)
+
+    else: # POST
+        if ad_id == 0:
+            form = SeekingAdForm(request.POST)
+        else:
+            ad = get_object_or_404(SeekingAd, id=ad_id, owner=request.user)
+            form = SeekingAdForm(request.POST, instance=ad)
+
+    # Was a GET, or Form was not valid
+    data = {
+        "form": form,
+    }
+
+    return render(request, "seeking_ad.html", data)
