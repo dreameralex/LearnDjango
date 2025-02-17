a631eed095eaab62d0277f8cadf7904ff272147b
Alex's PC <zhangyh8216@sina.com>
2025-02-17 23:51:08 +0800

    Finished Chapter7

--- bands/urls.py
+++ bands/urls.py
@@ -10,6 +10,11 @@ urlpatterns = [
     path('band/<int:band_id>/', views.band, name="band"),
     path('venues/', views.venues, name="venues"),
     path('add_venue/', views.edit_venue, name="add_venue"),
+    path('edit_venue/<int:venue_id>', views.edit_venue, name="edit_venue"),
     path('restricted_page/', views.restricted_page, name="restricted_page"),
     path('musician_restricted/<int:musician_id>/', views.musician_restricted, name="musician_restricted"),
+    path(
+        "venues_restricted/", views.venues_restricted, name="venues_restricted"
+    ),
+
 ]
